540d6db842397730b52e501a6341a20d25c0f403
David Pachura <david.pachura@gmail.com>
2025-12-22 15:48:17 -0600

    add tutorial resource

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,19 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people)))
-        .run();
+    .add_plugins(DefaultPlugins)
+    .add_plugins(HelloPlugin)
+    .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(5.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
+    }
 }
 
 #[derive(Component)]
@@ -18,13 +28,15 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Nicholas Romanov".to_string())));
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
